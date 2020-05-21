94798bfd576a1d56f73cf8f279c1086fde97ca50
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2020-05-20 21:19:08 -0300

    Adicionado validações para realizar o cadastro de usuário

--- BlaBlaTec/src/app/cadastrar/cadastrar.page.ts
+++ BlaBlaTec/src/app/cadastrar/cadastrar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 import { UserService } from '../services/user/user.service';
 
@@ -13,17 +14,87 @@ export class CadastrarPage implements OnInit {
     Nome: '',
     Email:  '',
     Senha: '',
-    ConfirmacaoSenha: ''
+    ConfirmacaoSenha: '',
+    Ra: '',
+    Sobrenome: '',
+    ConcordaComTermos: false
   };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+              private alertController: AlertController,
+              private navCtrl: NavController) {}
   ngOnInit() { 
 
    }
 
   finalizarCadastro(){
-    this.userService.cadastrarUsuario(this.usuario).subscribe(data =>{
-      console.log(data);
+    if(!this.verificarSenha()){
+      this.exibirMensagemConformacaoSenha();
+      return;
+    }else{
+      if(this.usuario.ConcordaComTermos){
+        this.userService.cadastrarUsuario(this.usuario).subscribe(
+          data =>{ this.exibirMensagemCadastroRealizado(); },
+          error => {this.exibirMensagemCadastroComErro()}
+          );
+      }
+      else{
+        this.exibirMensagemConcordaComTermos();
+        return;
+      }
+    }
+  }
+
+  async exibirMensagemCadastroRealizado() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      message: 'Cadastro realizado com sucesso',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          this.navCtrl.navigateRoot('home');
+        }
+      }]
     });
+
+    await alert.present();
+  }
+
+  async exibirMensagemCadastroComErro() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      message: 'Houve um problema ao realizar o cadastro. Tente novamente mais tarde.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async exibirMensagemConformacaoSenha() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      message: 'A confirmação de senha está diferente da senha digitada.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async exibirMensagemConcordaComTermos() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      message: 'Para realizar o cadastro, você deve concordar com nossos termos de uso.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  verificarSenha(){
+    if(this.usuario.Senha != this.usuario.ConfirmacaoSenha){
+      return false;
+    }else{
+      return true;
+    }
   }
 }
