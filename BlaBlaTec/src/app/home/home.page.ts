6c06ab6d92a0f2abaffa8a52f702d411abc87a11
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2020-05-20 22:16:52 -0300

    Adicionado rotina para efetuar Login

--- BlaBlaTec/src/app/home/home.page.ts
+++ BlaBlaTec/src/app/home/home.page.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
+import { UserService } from '../services/user/user.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {}
+  usuario = {
+    Ra:  '',
+    Password: ''
+  };
+  constructor(public navCtrl: NavController, private userService: UserService) {}
 
   acessarCadastrar(): void {
     this.navCtrl.navigateRoot('cadastrar');
   }
 
-  /*    // Chamar Página do login  (acesso ao banco)
-//TAMBÉM NA ALTERAR HOME.PAGE.HTML  para <ion-button expand="block"(click)='acessarCadastrar()'>Acessar</ion-button>
-  acessarLogin(): void{
-    this.navCtrl.navigateRoot('');
-  } */
+  realizarLogin(): void {
+    this.userService.autenticarUsuario(this.usuario).subscribe((data: Response) => {
+      this.navCtrl.navigateRoot('mapas');
+  },
+  (error: any) => {
+      console.log(error);
+  });
+  }
 }
