38abfc5c6ff343174baa85e1d3b8d6c7a14dcc14
Alan Monteiro <alan.hiwatari@gmail.com>
2020-06-18 23:10:03 -0300

    Adicionado Ion-Loading na Home

--- BlaBlaTec/src/app/home/home.page.ts
+++ BlaBlaTec/src/app/home/home.page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from '@ionic/angular';
-
+import { NavController, AlertController, LoadingController } from '@ionic/angular';
 
 import { UserService } from '../services/user/user.service';
-
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -12,31 +11,53 @@ import { UserService } from '../services/user/user.service';
 })
 export class HomePage {
   usuario = {
-    Ra:  '',
-    Password: ''
+    Ra: '',
+    Password: '',
   };
-  constructor(public navCtrl: NavController, private userService: UserService, private alertController: AlertController) {}
+
+  constructor(
+    public navCtrl: NavController,
+    private userService: UserService,
+    private alertController: AlertController,
+    private loadingCtrl: LoadingController
+  ) {}
 
   acessarCadastrar(): void {
     this.navCtrl.navigateRoot('cadastrar');
   }
 
-  realizarLogin(): void {
+  async realizarLogin() {
     // this.navCtrl.navigateRoot('mapas');
-    this.userService.autenticarUsuario(this.usuario).subscribe((data: any) => {
-      window.localStorage.setItem("ContentLocaly", data.accessToken);
-      this.navCtrl.navigateRoot('mapas');
-  },
-  (error: any) => {
-    console.log(error);
-    this.exibirMensagemErroLogin();
-  });
+
+    let carregando = await this.loadingCtrl.create({
+      message: 'Carregando...',
+    });
+
+    carregando.present();
+
+    this.userService
+      .autenticarUsuario(this.usuario)
+      .pipe(
+        finalize(() => {
+          carregando.dismiss();
+        })
+      )
+      .subscribe(
+        (data: any) => {
+          window.localStorage.setItem('ContentLocaly', data.accessToken);
+          this.navCtrl.navigateRoot('mapas');
+        },
+        (error: any) => {
+          console.log(error);
+          this.exibirMensagemErroLogin();
+        }
+      );
   }
   async exibirMensagemErroLogin() {
     const alert = await this.alertController.create({
       header: 'Aviso',
       message: 'Usuario ou senha incorreto.',
-      buttons: ['OK']
+      buttons: ['OK'],
     });
     await alert.present();
   }
