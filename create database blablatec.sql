699376585bcf6179ec6b5d070e0c665458b69d50
Johnatas Santos <johnatas.sf@gmail.com>
2020-06-20 10:03:48 -0300

    Alterado script blablatec

--- create database blablatec.sql
+++ create database blablatec.sql
@@ -1,14 +1,15 @@
-show databases;
+drop database blablatec;
 create database blablatec;
 use blablatec;
-create table veiculo(
+
+CREATE TABLE veiculo(
 id_veiculo integer primary key not null auto_increment,
 id_placa_veiculo char(8) not null,
 nm_modelo_veiculo varchar (20) not null,
 nm_cor_veiculo varchar (10) not null,
-qt_lugar_veiculo varchar (10) not null);
+qt_lugar_veiculo int (10) not null);
 
-create table usuario(
+CREATE TABLE usuario(
 id_usuario integer primary key not null auto_increment,
 nm_usuario varchar (100) not null,
 nm_sobrenome_usuario varchar (100) not null,
@@ -19,75 +20,56 @@ cd_telefone_usuario char (11) not null,
 im_usuario blob ,
 cd_ra_usuario varchar(20) not null,
 id_veiculo integer,
-CONSTRAINT fk_id_placa_veiculo FOREIGN KEY (id_veiculo) references veiculo(id_veiculo);
+CONSTRAINT fk_id_placa_veiculo FOREIGN KEY (id_veiculo) references veiculo(id_veiculo));
 
-
-create table viagem(
+CREATE TABLE viagem(
 id_viagem integer primary key not null auto_increment,
-nm_origem_viagem varchar (100) not null,
-nm_destino_viagem varchar (100) not null);
-
-create table avaliacao(
- id_avaliacao integer primary key not null auto_increment,
- vl_avaliacao char (5) ,
-id_usuario_avaliador integer,
-id_usuario_avaliado integer,
+nm_origem_viagem varchar (500) not null,
+nm_destino_viagem varchar (500) not null,
 id_item_viagem integer,
-CONSTRAINT fk_avaliador FOREIGN KEY (id_usuario_avaliador) references usuario(id_usuario),
-CONSTRAINT fk_avaliado FOREIGN KEY (id_usuario_avaliado) references usuario(id_usuario),
-CONSTRAINT fk_item_viagem FOREIGN KEY (id_item_viagem) references itemViagem(id_item_viagem)
+dt_viagem datetime not null,
+dt_finalizacao datetime,
+id_usuario_motorista integer,
+qt_lugar_disponivel int,
+vl_viagem decimal(10,2),
+constraint fk_usuario_motorista foreign key (id_usuario_motorista) references usuario(id_usuario)
 );
- 
- create table avaliacaoPlataforma(
+
+CREATE TABLE itemViagem(
+ id_item_viagem integer primary key not null auto_increment,
+ cd_latitude_inicial varchar(200) not null,
+ cd_longitude_final varchar(200) not null,
+ id_viagem integer,
+CONSTRAINT fk_viagem FOREIGN KEY (id_viagem) references viagem(id_viagem)
+);
+
+CREATE TABLE usuarioCaronaViagem(
+id_usuario_carona_viagem integer primary key not null auto_increment,
+id_item_viagem integer,
+id_usuario integer,
+constraint fk_item_viagem foreign key (id_item_viagem) references itemViagem(id_item_viagem)
+
+);
+
+CREATE TABLE avaliacaoPlataforma(
  id_avaliacao_plataforma integer primary key not null auto_increment,
- vl_avaliacao_plataforma char (5),
+ vl_avaliacao char (5) ,
 id_usuario integer,
 CONSTRAINT fk_plataforma FOREIGN KEY (id_usuario) references usuario(id_usuario));
- 
- create table itemViagem(
- id_item_viagem integer primary key not null auto_increment,
- dt_viagem date not null,
- hr_viagem time not null,
- vl_viagem double (8,2) not null,
-id_usuario_carona integer,
-id_usuario_motorista integer,
-CONSTRAINT fk_carona FOREIGN KEY (id_usuario_carona) references usuario(id_usuario),
-CONSTRAINT fk_motorista FOREIGN KEY (id_usuario_motorista) references usuario(id_usuario)
-);
 
-select*from usuario;
+CREATE TABLE rotasAtivas(
+	id_rotas_ativas integer primary key auto_increment,
+    cd_latitude_atual varchar(200) not null,
+    cd_longitude_atual varchar(200) not null,
+    id_item_viagem integer not null,
+    CONSTRAINT fk_rotas_item_viagem FOREIGN KEY (id_item_viagem) REFERENCES itemViagem(id_item_viagem)); 
 
-INSERT INTO veiculo(id_veiculo,id_placa_veiculo, nm_modelo_veiculo,nm_cor_veiculo, qt_lugar_veiculo) VALUES
-(NULL,'12345678', 'volkswagen', 'branco', 8);
-
-select*from veiculo;
-
-
-INSERT INTO usuario (id_usuario,nm_usuario,nm_sobrenome_usuario, nm_email_usuario,cd_passwordsalt, cd_passwordhash, cd_telefone_usuario, im_usuario, cd_ra_usuario) VALUES
-(NULL, 'Jose Roberto','da Silva', 'jose@gmail.com',NULL, NULL, 13974856393, 'photo', '1234567897458' );
-
-select*from usuario;
-
-INSERT INTO viagem (id_viagem,nm_origem_viagem, nm_destino_viagem) 
-VALUES (null, 'São Vicente', 'Praia Grande');
-
-select*from viagem;
-
-
-INSERT INTO avaliacao (id_avaliacao,vl_avaliacao, id_usuario_avaliador, id_usuario_avaliado, id_item_viagem) 
-VALUES (null, 5 , null, null, null);
-
-SELECT * FROM avaliacao;
-
-INSERT INTO avaliacaoplataforma (id_avaliacao_plataforma,vl_avaliacao,id_usuario) 
-  VALUES (null, 5, null);
-
-SELECT * FROM avaliacaoplataforma;
-
-INSERT INTO itemViagem ( id_item_viagem, dt_viagem, hr_viagem, vl_viagem, id_usuario_carona, id_usuario_motorista) 
-VALUES (null, '20200521', '12:23:06', 25.30, null, null);
-
-SELECT*FROM itemviagem;
-
+CREATE TABLE solicitacaoViagem(
+		id_solicitacao_viagem integer primary key auto_increment,
+		id_usuario_carona integer, 
+		id_viagem integer,
+		CONSTRAINT fk_sv_id_usuario FOREIGN KEY (id_usuario_carona) REFERENCES usuario(id_usuario),
+		CONSTRAINT fk_sv_id_viagem FOREIGN KEY (id_viagem) REFERENCES viagem(id_viagem)
+	);
 
 
